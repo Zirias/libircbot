774a8f0988ba92aaba5a33770606eea46768cb00
Felix Palmen <felix@palmen-it.de>
2022-10-14 09:25:46 +0200

    Bump lib version to 1.0.0

--- src/lib/ircbot/ircbot.mk
+++ src/lib/ircbot/ircbot.mk
@@ -31,8 +31,8 @@ ircbot_HEADERS_INSTALL:= 	decl \
 
 ircbot_LDFLAGS:=		-pthread
 ircbot_HEADERDIR:=		include$(PSEP)ircbot
-ircbot_V_MAJ:=			0
-ircbot_V_MIN:=			1
+ircbot_V_MAJ:=			1
+ircbot_V_MIN:=			0
 ircbot_V_REV:=			0
 
 $(call librules, ircbot)
